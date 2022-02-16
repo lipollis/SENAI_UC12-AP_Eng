67ecf2810773a9c154a2f3d5699f7f87654b4b9e
lipollis <aline.pollis@hotmail.com>
2022-02-15 22:44:35 -0300

    Start project

---
+++ src/main/java/com/ApEng/Ap_Engenharia/ApEngenhariaApplication.java
@@ -0,0 +1,13 @@
+package com.ApEng.Ap_Engenharia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApEngenhariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApEngenhariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ApEng/Ap_Engenharia/ApEngenhariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ApEng.Ap_Engenharia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApEngenhariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
